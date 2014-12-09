cdfacae9be052ee99b8a456227746dd63bd872bc
Lari Hotari <lhotari@pivotal.io>
2014-12-09 13:40:07 +0000

    add instructions how to dump threads

--- deadlock_in_debug.sh
+++ deadlock_in_debug.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
+echo 'Press CTRL-\ to dump threads in current console.'
 vagrant ssh -c "cd reactor && git reset --hard 08b83312 && ./gradlew :reactor-core:test --tests '*StreamIdentityProcessorTests.testIdentityProcessor'"
